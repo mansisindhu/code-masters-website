dc651309b33242a992a273f13eb1141688640a6d
mansisindhu <mansisindhu2909@gmail.com>
2021-01-21 17:59:55 +0530

    js for smooth scroll and fixed nav bar

--- script.js
+++ script.js
@@ -6,3 +6,29 @@
     $('.btn').toggleClass('change');
 
   })
+  //transparent background
+
+  $(window).scroll(function () {
+
+    let position = $(window).scrollTop();
+
+
+    if (position >= 100) {
+      $('nav, nav-container').addClass('navBackground')
+    } else {
+      $('nav, nav-container').removeClass('navBackground')
+
+    }
+
+  })
+  //smooth scroll
+
+  $('.nav-links a').click(function (link) {
+    link.preventDefault();
+
+    let target = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top
+    }, 3000);
+
+  })
